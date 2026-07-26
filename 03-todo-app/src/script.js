056b8f6e7cb2574240a3ca167670bc5ad8a82d99
vikram <vikramgond22052@gmail.com>
2026-07-26 23:21:17 +0530

    fix the issue with button click

--- 03-todo-app/src/script.js
+++ 03-todo-app/src/script.js
@@ -2,15 +2,21 @@ const todos = document.getElementById("todos");
 const addBtn = document.getElementById("add");
 const todoLists = document.getElementById("todo-lists");
 
-addBtn.addEventListener("click", createTodos());
-
-addBtn.addEventListener("keypress", (e) => {
-  if (e.key == "Enter") createTodos();
-});
-
-const createTodos = () => {
+const createTodos = (e) => {
   let todo = document.createElement("li");
-  todo.id = todo - list;
+  let deleteBtn = document.createElement("button");
+
+  deleteBtn.id = "deleteBtn";
+  todo.id = "todo-list";
+
+  deleteBtn.innerHTML = "Delete"
   todo.textContent = todos.value.trim();
+  todo.appendChild(deleteBtn);
   todoLists.appendChild(todo);
 };
+
+addBtn.addEventListener("click", createTodos);
+
+todos.addEventListener("keydown", (e) => {
+  if (e.key == "Enter") {createTodos()};
+});
