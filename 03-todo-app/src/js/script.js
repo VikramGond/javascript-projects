6216de615e8e23594627dd391089f5d633de3e1b
Vikram <vikramgond22052@gmail.com>
2026-08-09 10:07:41 +0530

    add unique key to every todo using crypto.randomUUID()

--- 03-todo-app/src/js/script.js
+++ 03-todo-app/src/js/script.js
@@ -2,21 +2,33 @@ const todoText = document.getElementById("todo-input");
 const addButton = document.querySelector(".add-todos");
 const todoListContainer = document.querySelector(".todo-lists");
 
+// todos array
+
+const todos = [];
+
 // function declaration
 
-const addNewTodo = (e) => {
+const addNewTodo = () => {
   const text = todoText.value.trim();
 
   if (text !== "") {
+    const todo = {
+      id: crypto.randomUUID(),
+      text: text,
+      completed: false,
+    };
+    todos.push(todo);
+
     const newLi = document.createElement("li");
     newLi.classList.add("todos-items");
+    newLi.dataset.id = todo.id;
 
     newLi.innerHTML = `
     <label>
         <input type="checkbox" class="input" />
         <span class="custom-checkbox"></span>
     </label>
-    <p class="item">${text}</p>
+    <p class="item">${todo.text}</p>
     <div>
         <button class="delete">Delete</button>
         <p class="time">${getCurrentTime()}</p>
