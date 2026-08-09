51c8b710d9ed5d220337e8f35a51dc75b4281031
Vikram <vikramgond22052@gmail.com>
2026-08-09 10:35:41 +0530

    create seprate function for each task, Using function based architechture

--- 03-todo-app/src/js/script.js
+++ 03-todo-app/src/js/script.js
@@ -2,44 +2,55 @@ const todoText = document.getElementById("todo-input");
 const addButton = document.querySelector(".add-todos");
 const todoListContainer = document.querySelector(".todo-lists");
 
-// todos array
+//---------------------------todos array------------------------------
 
 const todos = [];
 
-// function declaration
+//----------------------function declaration---------------------------
 
-const addNewTodo = () => {
+// validating todos
+const validateTodo = () => {
   const text = todoText.value.trim();
-
   if (text !== "") {
-    const todo = {
-      id: crypto.randomUUID(),
-      text: text,
-      completed: false,
-    };
-    todos.push(todo);
+    createTodo(text);
+  }
+};
 
-    const newLi = document.createElement("li");
-    newLi.classList.add("todos-items");
-    newLi.dataset.id = todo.id;
+//creating todo in todos variable
+const createTodo = (text) => {
+  const todo = {
+    id: crypto.randomUUID(),
+    text: text,
+    completed: false,
+  };
 
-    newLi.innerHTML = `
+  todos.push(todo);
+  fetchTodos(todo.text, todo.id);
+};
+
+//fetching todo at user site
+const fetchTodos = (text, id) => {
+  const newLi = document.createElement("li");
+  newLi.classList.add("todos-items");
+  newLi.dataset.id = id;
+
+  newLi.innerHTML = `
     <label>
         <input type="checkbox" class="input" />
         <span class="custom-checkbox"></span>
     </label>
-    <p class="item">${todo.text}</p>
+    <p class="item">${text}</p>
     <div>
         <button class="delete">Delete</button>
         <p class="time">${getCurrentTime()}</p>
     </div>`;
 
-    todoListContainer.appendChild(newLi);
+  todoListContainer.appendChild(newLi);
 
-    todoText.value = "";
-  }
+  todoText.value = "";
 };
 
+//getting exact time from user site
 const getCurrentTime = () => {
   const now = new Date();
   const hours = String(now.getHours()).padStart(2, "0");
@@ -47,11 +58,14 @@ const getCurrentTime = () => {
   return `${hours}:${minutes}`;
 };
 
-// event Listeners
-addButton.addEventListener("click", addNewTodo);
+// --------------------event Listeners--------------------------
 
+// on button press
+addButton.addEventListener("click", validateTodo);
+
+// on key press
 todoText.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
-    addNewTodo();
+    validateTodo();
   }
 });
