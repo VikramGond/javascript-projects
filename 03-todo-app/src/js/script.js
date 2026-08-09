e14b65f8ac5e62d0e44915c5dfa3f00c097431ed
Vikram <vikramgond22052@gmail.com>
2026-08-09 13:22:34 +0530

    fix unwanted rendering

--- 03-todo-app/src/js/script.js
+++ 03-todo-app/src/js/script.js
@@ -1,153 +1,155 @@
-const todoText = document.getElementById("todo-input");
-const addButton = document.querySelector(".add-todos");
-const todoListContainer = document.querySelector(".todo-lists");
+  const todoText = document.getElementById("todo-input");
+  const addButton = document.querySelector(".add-todos");
+  const todoListContainer = document.querySelector(".todo-lists");
 
-//---------------------------todos array------------------------------
+  //---------------------------todos array------------------------------
 
-let todos = [];
+  let todos = [];
 
-//----------------------function declaration---------------------------
+  //----------------------function declaration---------------------------
 
-// validating todos
-const validateTodo = () => {
-  const text = todoText.value.trim();
-  if (text !== "") {
-    createTodo(text);
-  }
-};
-
-//creating todo in todos variable
-const createTodo = (text) => {
-  const todo = {
-    id: crypto.randomUUID(),
-    text,
-    completed: false,
-    time: getCurrentTime()
+  // validating todos
+  const validateTodo = () => {
+    const text = todoText.value.trim();
+    if (text !== "") {
+      createTodo(text);
+    }
   };
 
-  todos.push(todo);
-  saveTodos();
-  renderTodos(todo);
-};
+  //creating todo in todos variable
+  const createTodo = (text) => {
+    const todo = {
+      id: crypto.randomUUID(),
+      text,
+      completed: false,
+      time: getCurrentTime()
+    };
 
-//fetching todo at user site
-const renderTodos = (todoOBJ) => {
-  const newLi = document.createElement("li");
-  newLi.classList.add("todos-items");
-  newLi.dataset.id = todoOBJ.id;
-
-  newLi.innerHTML = `
-    <label>
-        <input type="checkbox" 
-               class="input" 
-               ${todoOBJ.completed ? "checked" : ""} />
-        <span class="custom-checkbox"></span>
-    </label>
-    <p class="item">${todoOBJ.text}</p>
-    <div>
-        <button class="delete">Delete</button>
-        <p class="time">${todoOBJ.time}</p>
-    </div>`;
-
-  todoListContainer.appendChild(newLi);
-
-  if(todoOBJ.completed) {
-    newLi.classList.toggle("completed");
-  }
-
-  todoText.value = "";
-};
-
-//getting exact time from user site
-const getCurrentTime = () => {
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  return `${hours}:${minutes}`;
-};
-
-//handles todo completion
-const handleTodoCompletion = (todoElement, completed) => {
-  const todoId = todoElement.dataset.id;
-  const todo = todos.find((todo) => todo.id === todoId);
-
-  todo.completed = completed;
-  todoElement.classList.toggle("completed", todo.completed);
-  saveTodos();
-};
-
-//function for deleting todos
-const handleTodoDelete = (todoElement) => {
-  const todoId = todoElement.dataset.id;
-
-  todos = todos.filter((todo) => todo.id !== todoId);
-  todoElement.remove();
-  saveTodos();
-};
-
-// saveTodos
-const saveTodos = () => {
-  localStorage.setItem("todos", JSON.stringify(todos));
-};
-
-//load saved todos
-const loadSavedTodos = () => {
-  const savedTodos = localStorage.getItem("todos");
-
-  if (savedTodos) {
-    todos = JSON.parse(savedTodos);
-    renderSavedTodos();
-  }
-};
-
-//render todo after page reload
-const renderSavedTodos = () => {
-  todos.forEach((todo) => {
+    todos.push(todo);
+    saveTodos();
     renderTodos(todo);
+    
+    //CLEARING THE INPUT VALUE
+    todoText.value = "";
+  };
+
+  //fetching todo at user site
+  const renderTodos = (todoOBJ) => {
+    const newLi = document.createElement("li");
+    newLi.classList.add("todos-items");
+    newLi.dataset.id = todoOBJ.id;
+
+    newLi.innerHTML = `
+      <label>
+          <input type="checkbox" 
+                class="input" 
+                ${todoOBJ.completed ? "checked" : ""} />
+          <span class="custom-checkbox"></span>
+      </label>
+      <p class="item">${todoOBJ.text}</p>
+      <div>
+          <button class="delete">Delete</button>
+          <p class="time">${todoOBJ.time}</p>
+      </div>`;
+
+    todoListContainer.appendChild(newLi);
+
+    if(todoOBJ.completed) {
+      newLi.classList.toggle("completed");
+    }
+
+  };
+
+  //getting exact time from user site
+  const getCurrentTime = () => {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    return `${hours}:${minutes}`;
+  };
+
+  //handles todo completion
+  const handleTodoCompletion = (todoElement, completed) => {
+    const todoId = todoElement.dataset.id;
+    const todo = todos.find((todo) => todo.id === todoId);
+
+    todo.completed = completed;
+    todoElement.classList.add("completed", todo.completed);
+    saveTodos();
+  };
+
+  //function for deleting todos
+  const handleTodoDelete = (todoElement) => {
+    const todoId = todoElement.dataset.id;
+
+    todos = todos.filter((todo) => todo.id !== todoId);
+    todoElement.remove();
+    saveTodos();
+  };
+
+  // saveTodos
+  const saveTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  //load saved todos
+  const loadSavedTodos = () => {
+    const savedTodos = localStorage.getItem("todos");
+
+    if (savedTodos) {
+      todos = JSON.parse(savedTodos);
+      renderSavedTodos();
+    }
+  };
+
+  //render todo after page reload
+  const renderSavedTodos = () => {
+    todos.forEach((todo) => {
+      renderTodos(todo);
+    });
+  };
+
+  // --------------------event Listeners--------------------------
+
+  // on button press
+  addButton.addEventListener("click", validateTodo);
+
+  // on key press
+  todoText.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      validateTodo();
+    }
   });
-};
 
-// --------------------event Listeners--------------------------
+  // checkbox
+  todoListContainer.addEventListener("change", (e) => {
+    if (e.target.classList.contains("input")) {
+      const todoElement = e.target.closest(".todos-items");
 
-// on button press
-addButton.addEventListener("click", validateTodo);
+      handleTodoCompletion(todoElement, e.target.checked);
+    }
+  });
 
-// on key press
-todoText.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    validateTodo();
-  }
-});
+  //text
+  todoListContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("item")) {
+      const todoElement = e.target.closest(".todos-items");
+      const checkbox = todoElement.querySelector(".input");
 
-// checkbox
-todoListContainer.addEventListener("change", (e) => {
-  if (e.target.classList.contains("input")) {
-    const todoElement = e.target.closest(".todos-items");
+      checkbox.checked = !checkbox.checked;
 
-    handleTodoCompletion(todoElement, e.target.checked);
-  }
-});
-
-//text
-todoListContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("item")) {
-    const todoElement = e.target.closest(".todos-items");
-    const checkbox = todoElement.querySelector(".input");
-
-    checkbox.checked = !checkbox.checked;
-
-    handleTodoCompletion(todoElement, checkbox.checked);
-  }
-});
+      handleTodoCompletion(todoElement, checkbox.checked);
+    }
+  });
 
 
-//event listener for delete
-todoListContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("delete")) {
-    const todoElement = e.target.closest(".todos-items");
+  //event listener for delete
+  todoListContainer.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete")) {
+      const todoElement = e.target.closest(".todos-items");
 
-    handleTodoDelete(todoElement);
-  }
-});
+      handleTodoDelete(todoElement);
+    }
+  });
 
-loadSavedTodos();
+  loadSavedTodos();
